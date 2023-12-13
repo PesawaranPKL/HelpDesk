aacf047b6171c8940f215fd69a60b7039da9095b
Siti Kholisah Aulia <125333448+sk-aulia@users.noreply.github.com>
2023-12-13 13:21:42 +0700

    seeder_update
    noto :')

---
+++ app/Database/Seeds/Notifikasi.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Notifikasi extends Seeder
+{
+    public function run()
+	{
+		$notifikasi_data = [
+			[
+                'id_pengaduan'  => 1
+			],
+		];
+
+		foreach($notifikasi_data as $data){
+			$this->db->table('notifikasi')->insert($data);
+		}
+
+	}
+}
